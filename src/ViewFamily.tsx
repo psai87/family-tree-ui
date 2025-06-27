e7ba43fdb9dd89cf8a7d5248cbc7fdd0cdb90c40
psai87 <PSAI87@GMAIL.COM>
2025-06-27 10:16:57 -0700

    treeview

--- src/ViewFamily.tsx
+++ src/ViewFamily.tsx
@@ -1,9 +1,81 @@
-function ViewFamily() {
-    return (
-        <>
+import {useCallback} from 'react';
+import {
+    ReactFlow,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    MiniMap,
+    Controls,
+} from '@xyflow/react';
 
-        </>
-    )
+import '@xyflow/react/dist/base.css';
+
+import PeopleNode from './nodes/PeopleNode';
+
+function ViewFamily() {
+
+    const nodeTypes = {
+        peopleNode: PeopleNode,
+    };
+
+    const initNodes = [
+        {
+            id: '1',
+            type: 'peopleNode',
+            data: {name: 'sai shravan peravali', job: 'CEO', emoji: '😎'},
+            position: {x: 0, y: 50},
+        },
+        {
+            id: '2',
+            type: 'peopleNode',
+            data: {name: 'Tyler Weary', job: 'Designer', emoji: '🤓'},
+
+            position: {x: -200, y: 200},
+        },
+        {
+            id: '3',
+            type: 'peopleNode',
+            data: {name: 'Kristi Price', job: 'Developer', emoji: '🤩'},
+            position: {x: 200, y: 200},
+        },
+    ];
+
+    const initEdges = [
+        {
+            id: 'e1-2',
+            source: '1',
+            target: '2',
+        },
+        {
+            id: 'e1-3',
+            source: '1',
+            target: '3',
+        },
+    ];
+
+    const [nodes, , onNodesChange] = useNodesState(initNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initEdges);
+
+    const onConnect = useCallback(
+        (params: any) => setEdges((eds) => addEdge(params, eds)),
+        [],
+    );
+
+    return (
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            nodeTypes={nodeTypes}
+            fitView
+            className="bg-teal-50"
+        >
+            <MiniMap/>
+            <Controls/>
+        </ReactFlow>
+    );
 }
 
 export default ViewFamily
---
+++ src/nodes/PeopleNode.tsx
@@ -0,0 +1,45 @@
+import {Component, memo} from 'react';
+import {Handle, Position} from '@xyflow/react';
+
+class PeopleNode extends Component<{ data: any }> {
+    render() {
+        let {data} = this.props;
+        return (
+            <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400">
+                <div className="flex">
+                    <div className="rounded-full w-12 h-12 flex justify-center items-center bg-gray-100">
+                        {data.emoji}
+                    </div>
+                    <div className="ml-2">
+                        <div className="text-xs font-bold break-words whitespace-normal">{data.name}</div>
+                        <div className="text-xs text-gray-500">{data.job}</div>
+                        <div className="text-xs text-gray-500">{'email'}</div>
+                    </div>
+                </div>
+
+                <Handle
+                    type="target"
+                    position={Position.Top}
+                    className="w-12 !bg-teal-500"
+                />
+                {/*<Handle*/}
+                {/*    type="source"*/}
+                {/*    position={Position.Bottom}*/}
+                {/*    className="w-12 !bg-teal-500"*/}
+                {/*/>*/}
+                <Handle
+                    type="source"
+                    position={Position.Right}
+                    className="h-12 !bg-teal-500"
+                />
+                <Handle
+                    type="source"
+                    position={Position.Left}
+                    className="h-12 !bg-teal-500"
+                />
+            </div>
+        );
+    }
+}
+
+export default memo(PeopleNode);
